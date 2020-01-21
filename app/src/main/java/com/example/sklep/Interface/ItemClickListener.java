55a69f2d5259e75fde81700bdf081b2734627ae2
Mateusz <mateuszd2411@gmail.com>
2020-01-21 08:03:14 +0100

    Sklep java android studio

---
+++ app/src/main/java/com/example/sklep/Interface/ItemClickListener.java
@@ -0,0 +1,14 @@
+package com.example.sklep.Interface;
+
+import android.view.View;
+
+public interface ItemClickListener {
+
+    void onClick(View view, int position, boolean isLongClick);
+
+
+
+}
+
+
+
